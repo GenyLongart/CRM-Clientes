4aca6ed54d2a981b0600b01ed09630fa2dda8dc7
genesis <gnsslr@gmail.com>
2022-09-12 11:38:27 -0300

    Fixing routes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -14,10 +14,10 @@ const Layout = () => {
             
             <nav className='mt-10 '>
                 <Link 
-                className={`${actualUrl === '/clients' ? 'text-blue-300' : 'text-white'}  text-2xl block mt-2 hover:text-blue-300`} 
+                className={`${actualUrl === '/' ? 'text-blue-300' : 'text-white'}  text-2xl block mt-2 hover:text-blue-300`} 
                 to="/clients">Clientes</Link>
                 <Link 
-                className={`${actualUrl === '/clients/new' ? 'text-blue-300' : 'text-white'}  text-2xl block mt-2 hover:text-blue-300`} 
+                className={`${actualUrl === '/new' ? 'text-blue-300' : 'text-white'}  text-2xl block mt-2 hover:text-blue-300`} 
                 to="/clients/new">Nuevo Cliente</Link>
             </nav>
         </div>
